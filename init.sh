d6f3efd35a794a531a26d75f3258541aac91533c
taiseiKMC <taisei@kmc.gr.jp>
2018-06-10 10:57:27 +0900

    Fix init.sh
    台詞再生をコメントアウトした

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ cookie_line_cnt=0
 function _update_psvar() {
   cookie_line_cnt=`expr $cookie_line_cnt \% 402 + 1`
   psvar[1]=`sed -n ${cookie_line_cnt}p ${cookie_path}/lines.txt`
-  printf "%s/voices/%04d\n" $cookie_path $cookie_line_cnt  | xargs ${cookie_path}/play.sh
+  #printf "%s/voices/%04d\n" $cookie_path $cookie_line_cnt  | xargs ${cookie_path}/play.sh
 }
 autoload -Uz add-zsh-hook
 add-zsh-hook precmd _update_psvar
